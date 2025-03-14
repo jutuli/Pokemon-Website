89333a55b4db84648d3530b6d323c46e4d933f37
[PeterWolf242] <[peter.ebm@web.de]>
2025-03-14 09:14:10 +0100

    adjust responsive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,74 +1,92 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 import './Header.css';
 import ThemeToogle from '../ThemeToogle/ThemeToogle';
-import { PokemonTypes } from '../PokemonTypes/PokemonTypes';
+import { PokemonTypes } from '../PokemonTypes/POKEMONTYPES';
+import { mainContext } from '../../context/MainProvider';
 
 export default function Header() {
-  //* State für das Logo und den Box-Shadow
-  const [hideLogo, setHideLogo] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
+	//* State für das Logo und den Box-Shadow
+	const [hideLogo, setHideLogo] = useState(false);
+	const [scrolled, setScrolled] = useState(false);
 
-  //* Überwachen des Scroll-Events
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setHideLogo(true);
-        setScrolled(true); // Fügt den Box-Shadow hinzu
-      } else {
-        setHideLogo(false);
-        setScrolled(false); // Entfernt den Box-Shadow
-      }
-    };
+	//* Context für Pokemon-Suche
+	const context = useContext(mainContext);
+	if (!context) return null;
+	const { setSearchTerm, setSelectedTypeId } = context;
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+	//* useref für Search-Input-Feld
+	const searchRef = useRef<HTMLInputElement>(null);
 
-  //* State zum Öffnen & Schließen des Menüs
-  const [menu, setMenu] = useState<boolean>(false);
+	//* Überwachen des Scroll-Events
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 50) {
+				setHideLogo(true);
+				setScrolled(true); // Fügt den Box-Shadow hinzu
+			} else {
+				setHideLogo(false);
+				setScrolled(false); // Entfernt den Box-Shadow
+			}
+		};
 
-  // Menü sanft ein- & ausblenden
-  const toggleMenu = () => {
-    setMenu((prev) => !prev);
-  };
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 
-  return (
-    <header className={scrolled ? 'scrolled' : ''}>
-      <ThemeToogle />
+	//* State zum Öffnen & Schließen des Menüs
+	const [menu, setMenu] = useState<boolean>(false);
 
-      <div className="logo-input-box">
-        <div className={`logo ${hideLogo ? 'hide-logo' : ''}`}>
-          <img
-            src="/src/assets/pokemon-logo.png"
-            alt="Pokemon-Logo"
-            title="Pokemon-Logo"
-            aria-label="Pokemon-Logo"
-          />
-        </div>
-        <div className="header-input-section">
-          <input
-            className="input-search"
-            type="text"
-            placeholder="Search Pokemon"
-          />
-          <button className="btn-search" type="button">
-            Search
-          </button>
-        </div>
-      </div>
+	// Menü sanft ein- & ausblenden
+	const toggleMenu = () => {
+		setMenu((prev) => !prev);
+	};
 
-      {/* Menü-Button zum Öffnen des Pokémon-Typen-Menüs */}
-      <img
-        className="burger-menu"
-        src="/src/assets/hamburger-menu.png"
-        alt="Hamburger-Menu-Icon"
-        title="Hamburger-Menu-Icon"
-        aria-label="Hamburger-Menu-Icon"
-        onClick={toggleMenu}
-      />
+	//* Suchfunktion
+	const handleSearch = ((event: React.FormEvent) => {
+		event.preventDefault();
+		const inputString = searchRef.current?.value || "";
+		setSearchTerm(inputString);
+	})
 
-      {/* Pokémon-Typen-Menü wird nur gerendert, wenn `menu === true` */}
-      {menu && <PokemonTypes closeMenu={toggleMenu} />}
-    </header>
-  );
+	return (
+		<header className={scrolled ? 'scrolled' : ''}>
+			<ThemeToogle />
+
+			<div className="logo-input-box">
+				<div className={`logo ${hideLogo ? 'hide-logo' : ''}`}>
+					<img
+						src="/src/assets/pokemon-logo.png"
+						alt="Pokemon-Logo"
+						title="Pokemon-Logo"
+						aria-label="Pokemon-Logo"
+					/>
+				</div>
+				<form action="" onSubmit={handleSearch}>
+					<div className="header-input-section">
+						<input ref={searchRef}
+							className="input-search"
+							type="text"
+							placeholder="Search Pokemon"
+						/>
+						<button className="btn-search" type="submit">
+							Search
+						</button>
+					</div>
+				</form>
+			</div>
+
+			{/* Menü-Button zum Öffnen des Pokémon-Typen-Menüs */}
+			<img
+				className="burger-menu"
+				src="/src/assets/hamburger-menu.png"
+				alt="Hamburger-Menu-Icon"
+				title="Hamburger-Menu-Icon"
+				aria-label="Hamburger-Menu-Icon"
+				onClick={toggleMenu}
+			/>
+
+			{/* Pokémon-Typen-Menü wird nur gerendert, wenn `menu === true` */}
+			{menu && <PokemonTypes closeMenu={toggleMenu} />}
+		</header>
+	);
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,69 +5,72 @@ import './home.css';
 import PokemonCard from '../../components/PokemonCard/PokemonCard';
 
 interface Pokemon {
-  name: string;
-  url: string;
+	name: string;
+	url: string;
 }
 
 interface PokemonDetails {
-  id: number;
-  name: string;
-  sprites: {
-    front_default: string;
-  };
+	id: number;
+	name: string;
+	sprites: {
+		front_default: string;
+	};
 }
 
 const Home = () => {
-  const [pokemonList, setPokemonList] = useState<any[]>([]);
-  const context = useContext(mainContext);
-  if (!context) return null;
-  const { searchTerm, selectedTypeId, setSelectedPokemon } = context;
+	const [pokemonList, setPokemonList] = useState<any[]>([]);
+	const context = useContext(mainContext);
+	if (!context) return null;
+	const { searchTerm, selectedTypeId, setSelectedPokemon } = context;
 
-  // Pokemon holen (alle oder spezifisch durch SearchTerm oder Type gefiltert)
-  useEffect(() => {
-    const fetchPokemonList = async () => {
-      let url = 'https://pokeapi.co/api/v2/pokemon';
-      if (searchTerm) {
-        url = `https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`;
-      } else if (selectedTypeId) {
-        url = `https://pokeapi.co/api/v2/type/${selectedTypeId}`;
-      }
-      try {
-        const resp = await axios.get(url);
-        if (resp.data.results) {
-          setPokemonList(resp.data.results);
-        }
-      } catch (error) {
-        console.error('Error while Fetching all Pokemon:', error);
-        return;
-      }
-    };
-    fetchPokemonList();
-  }, [searchTerm, selectedTypeId]);
+	// Pokemon holen (alle oder spezifisch durch SearchTerm oder Type gefiltert)
+	useEffect(() => {
+		const fetchPokemonList = async () => {
+			let url = 'https://pokeapi.co/api/v2/pokemon';
+			if (searchTerm) {
+				url = `https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`;
+			} else if (selectedTypeId) {
+				url = `https://pokeapi.co/api/v2/type/${selectedTypeId}`;
+			}
+			try {
+				const resp = await axios.get(url);
+				if (resp.data.results) {
+					
+					setPokemonList(resp.data.results);
+				}
+				console.log(resp.data);
+			} catch (error) {
+				console.error('Error while Fetching all Pokemon:', error);
+				return;
+			}
 
-  return (
-    <section className="home">
-      {pokemonList.length === 0 && (
-        <div>
-          <h2>No Pokemon found 😢</h2>
-          <p>
-            Try searching for a different Pokemon name or select a different
-            type.
-          </p>
-        </div>
-      )}
-      <ul>
-        {pokemonList.map((pokemon: Pokemon) => {
-          const id = pokemon.url.split('/').filter(Boolean).pop(); // ID aus URL holen
-          if (!id) {
-            console.error('Error while getting ID from URL');
-            return null;
-          }
-          return <PokemonCard key={pokemon.name} id={id} pokemon={pokemon} />;
-        })}
-      </ul>
-    </section>
-  );
+		};
+		fetchPokemonList();
+	}, [searchTerm, selectedTypeId]);
+
+	return (
+		<section className="home">
+			{pokemonList.length === 0 && (
+				<div>
+					<h2>No Pokemon found 😢</h2>
+					<p>
+						Try searching for a different Pokemon name or select a different
+						type.
+					</p>
+				</div>
+			)}
+			<ul>
+				{pokemonList.map((pokemon: Pokemon) => {
+					const id = pokemon.url.split('/').filter(Boolean).pop(); // ID aus URL holen
+					if (!id) {
+						console.error('Error while getting ID from URL');
+						return null;
+					}
+					return <PokemonCard key={pokemon.name} id={id} pokemon={pokemon} />;
+				})}
+			</ul>
+		</section>
+	);
 };
 
 export default Home;
